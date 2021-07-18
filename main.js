e6e352b4239d7d5b71c7ec746668173daf10df32
ayush595 <81427649+ayush595@users.noreply.github.com>
2021-07-18 15:33:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+menu_list_array = ["Veg Margherita Pizza","Paneer Tikka Pizza","garlic pizza","supreme pizza","semizza","pasta pizza"];
+
+function getmenu(){
+var htmldata= ["menu_list_array"].innerHTML= "display_menu"; 
+
+}
+
+function add_item(){
+var htmldata= [];
+var item=document.getElementById("add_item").value;
+ 
+    
+
+
+}
+
+function add_top(){
+
+}
